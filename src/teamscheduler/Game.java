024b0864a1d99326677ccbb75bedae76ab6efbef
aHmAdaKrOuCh <90848204+aHmAdaKrOuCh@users.noreply.github.com>
2021-11-10 10:06:59 -0500

    finished

--- src/teamscheduler/Game.java
+++ src/teamscheduler/Game.java
@@ -9,18 +9,30 @@ package teamscheduler;
  * @author SPH_SHSM
  */
 public class Game {
+
     String home;
     String away;
     int homeScore;
     int awayScore;
+    String winner;
 
     public Game(String home, String away, int homeScore, int awayScore) {
         this.home = home;
         this.away = away;
         this.homeScore = homeScore;
         this.awayScore = awayScore;
+        this.winner = "";
     }
-    
-    
-    
+
+    public void UpdateScore(int score1, int score2) {
+        this.homeScore = score1;
+        this.awayScore = score2;
+        if (score1 > score2) {
+            this.winner = this.home;
+        } else {
+            this.winner = this.away;
+        }
+        System.out.println(this.winner + " is the winner!");
+    }
+
 }
--- src/teamscheduler/TeamScheduler.java
+++ src/teamscheduler/TeamScheduler.java
@@ -4,6 +4,8 @@
  */
 package teamscheduler;
 
+import java.util.*;
+
 /**
  *
  * @author SPH_SHSM
@@ -13,29 +15,107 @@ public class TeamScheduler {
     /**
      * @param args the command line arguments
      */
+    static Scanner in;
     public static void main(String[] args) {
         // TODO code application logic here
-        Team t1=new Team("Team 1");
-        Team t2=new Team("Team 2");
-        
-        Team[] teams={t1,t2};
-        Game g1=new Game(t1.name,t2.name,3,5);
+        in = new Scanner(System.in);
+        System.out.println("Welcome to the Champions league");
+        choices();
+        System.out.println("Give me the first team");
+        String input = in.nextLine();
+        Team t1 = new Team(input);
+        System.out.println("Give me the second team");
+        input = in.nextLine();
+        Team t2 = new Team(input);
+        System.out.println("Give me the third team");
+        input = in.nextLine();
+        Team t3 = new Team(input);
+        System.out.println("Give me the fourth team");
+        input = in.nextLine();
+        Team t4 = new Team(input);
+
+        Team[] teams = {t1, t2, t3, t4};
+
         Calendar schedule = new Calendar();
-        schedule.games.add(g1);
-        for (Team t:teams){
-            if (t.name.equals(g1.away)||t.name.equals(g1.home)){
-                t.addGame(g1);
+        schedule.games.add(new Game(t2.name, t1.name, 0, 0));
+        schedule.games.add(new Game(t3.name, t4.name, 0, 0));
+        schedule.games.add(new Game(t4.name, t2.name, 0, 0));
+        schedule.games.add(new Game(t4.name, t1.name, 0, 0));
+        schedule.games.add(new Game(t3.name, t1.name, 0, 0));
+        schedule.games.add(new Game(t4.name, t2.name, 0, 0));
+        schedule.games.add(new Game(t3.name, t2.name, 0, 0));
+        int choice;
+        do {
+            int counter = 1;
+            for (Game g : schedule.games) {
+                System.out.println(counter + ") " + g.home + " vs " + g.away);
+                counter++;
             }
-        } 
+            System.out.println("Which game do you want to enter scores for? -1 to quit");
+            choice = in.nextInt();
+            if (choice != -1) {
+                System.out.println("Enter the score for " + schedule.games.get(choice - 1).home);
+                int score = in.nextInt();
+                System.out.println("Enter the score for " + schedule.games.get(choice - 1).away);
+                int score2 = in.nextInt();
+                schedule.games.get(choice - 1).UpdateScore(score, score2);
+
+            }
+        } while (choice != -1);
         Stats(teams);
+//        if (t1score > t2score){
+//            System.out.println("Team 1 wins");
+//        }else{
+//            if (t1score < t2score)
+//                System.out.println("Team 2 wins");
+//        }
+//         if (t3score > t4score){
+//            System.out.println("Team 1 wins");
+//        }else{
+//            if (t3score < t4score)
+//                System.out.println("Team 2 wins");
+//    }
     }
-    
-    public static void Stats(Team[] teamList){
-        for (Team t:teamList){
+
+    public static void Stats(Team[] teamList) {
+        for (Team t : teamList) {
             System.out.println(t.name);
-            System.out.println("Wins: "+t.wins+"\tLosses: "+t.losses+"\tTies: "+t.ties);
-            System.out.println("Goals For: "+t.goalsFor+"\tGoals Against: "+t.goalsAgainst+"\n");
+            System.out.println("Wins: " + t.wins + "\tLosses: " + t.losses + "\tTies: " + t.ties);
+            System.out.println("Goals For: " + t.goalsFor + "\tGoals Against: " + t.goalsAgainst + "\n");
+
         }
     }
+
+    public static void choices() {
+        //Print out games without a winner
+        
+        int choice;
+        do {
+            System.out.println("Pick your choice");
+
+            choice = in.nextInt();
+
+            if (choice == 1) {
+                System.out.println("How many teams are you going to have?");
+                int teamNumber=in.nextInt();
+                scheduleGames(teamNumber);
+
+            } else if (choice == 2) {
+                updateScore();
+
+            } else if (choice == 4) {
+                System.out.println("Goodbye");
+            }
+        } while (choice!=4);
+
+    }
     
+    
+    public static void scheduleGames(int teams){
+        
+    }
+    
+    public static void updateScore(){
+    
+    }
 }
